f6735943bdc2ebd7f9e459847c68fa4483922f63
Chlamydomonos <2457242458@qq.com>
2022-09-26 13:46:26 +0800

    修改router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import TemplateEditView from '../views/TemplateEditView.vue';
+import RegisterView from '@/views/RegisterView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
