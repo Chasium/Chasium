f7b1cea2d563b92d75c54a2b95d6d0355f638fc2
Chlamydomonos <2457242458@qq.com>
2022-09-26 13:23:17 +0800

    安装sass，写了一部分TemplateEditView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,19 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import TemplateEditView from '../views/TemplateEditView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: "/",
-            name: "home",
+            path: '/',
+            name: 'home',
             component: HomeView,
+        },
+        {
+            path: '/template-edit',
+            name: 'template-edit',
+            component: TemplateEditView,
         },
     ],
 });
---
+++ frontend/src/views/TemplateEditView.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-container class="big-frame">
+        <el-header class="editor-header">test header</el-header>
+        <el-main>test main</el-main>
+    </el-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+class Data {}
+export default defineComponent({
+    data() {
+        return new Data();
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.big-frame {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+}
+
+.editor-header {
+    background-color: gray;
+}
+</style>
