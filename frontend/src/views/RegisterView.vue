1afde5097a7f6e3a804c618d89d1fd19962af043
Boinzz <Boinzz_bjf@613.com>
2022-09-27 16:03:01 +0800

    登录前端优化

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,38 +1,40 @@
 <template>
-    <div class="card register-title">
-        <h1>注&nbsp;册</h1>
-    </div>
-    <div class="register-main card">
-        <el-form label-width="75px" label-position="left">
-            <el-form-item label="用户名:" class="user-data">
-                <input v-model="userName" placeholder="请输入用户名" />
-            </el-form-item>
-            <el-form-item label="密码:" class="user-data">
-                <input
-                    type="password"
-                    v-model="password"
-                    show-password
-                    placeholder="请输入密码"
-                />
-            </el-form-item>
-            <el-form-item label="确认密码:" class="user-data">
-                <input
-                    type="password"
-                    v-model="confirmPassword"
-                    show-password
-                    placeholder="请再次输入密码"
-                />
-            </el-form-item>
-            <div class="register user-data">
-                <div class="container">
-                    <button class="button is-dark" @click="test">注册</button>
+    <el-container>
+        <div class="register-main card">
+            <div class="register-title">注 册</div>
+            <el-form label-width="75px" label-position="left">
+                <el-form-item label="用户名:" class="user-data">
+                    <input v-model="userName" placeholder="请输入用户名" />
+                </el-form-item>
+                <el-form-item label="密码:" class="user-data">
+                    <input
+                        type="password"
+                        v-model="password"
+                        show-password
+                        placeholder="请输入密码"
+                    />
+                </el-form-item>
+                <el-form-item label="确认密码:" class="user-data">
+                    <input
+                        type="password"
+                        v-model="confirmPassword"
+                        show-password
+                        placeholder="请再次输入密码"
+                    />
+                </el-form-item>
+                <div class="register user-data">
+                    <div class="container">
+                        <button class="button is-dark" @click="test">
+                            注册
+                        </button>
+                    </div>
                 </div>
+            </el-form>
+            <div class="back">
+                <router-link to="/">返回</router-link>
             </div>
-        </el-form>
-        <div class="back">
-            <router-link to="/">返回</router-link>
         </div>
-    </div>
+    </el-container>
 </template>
 
 <script lang="ts">
@@ -51,12 +53,19 @@ export default {
 
 <style lang="scss" scoped>
 @import 'bulma/bulma.sass';
+.el-container {
+    height: 100vh;
+    width: 100vw;
+    font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+        'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+}
 .register-main {
-    height: 16rem;
+    height: 20rem;
     width: 15rem;
     position: fixed;
-    top: calc((100vh - 20rem) * 2 / 5);
-    left: calc((100vw - 15rem) / 2);
+    top: calc((100vh - 20rem) * 3 / 7);
+    left: 50%;
+    transform: translate(-50%, 0);
     padding: 2rem;
 
     input {
@@ -68,20 +77,12 @@ export default {
 }
 .register-title {
     font-size: 36px;
-    line-height: 40px;
-    position: fixed;
-    width: 15rem;
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-bottom: 0.6rem;
-    padding-top: 0.6rem;
-    left: calc((100vw - 15rem) / 2);
-    top: calc((100vh - 25rem) * 2 / 5 - 40px);
+    padding-bottom: 1.5rem;
     text-align: center;
     font-weight: lighter;
 }
 .register {
-    margin-top: 1.5em;
+    margin-top: 2em;
     .container {
         width: fit-content;
         button {
