c66a86c5c658f8e1b20ab2abe36f42ca64260244
Boinzz <Boinzz_bjf@613.com>
2022-10-22 23:02:15 +0800

    修复apigen

--- apigen/main.py
+++ apigen/main.py
@@ -2,13 +2,15 @@
 入口文件
 """
 import re
+import shutil
 import glob
 import sys
 from src.models import ModelsException
 from src.generator import Generator
 
-TEST_PATH = re.compile(r'apis\\test\\')
+TEST_PATH = re.compile(r'apis[\\/]test[\\/]')
 XML_PATH = re.compile(r'.*\.xml')
+XML2_PATH = re.compile(r'[\\/].*?\.xml$')
 
 if __name__ == "__main__":
     # 遍历apis目录下除test目录外的所有xml文件，调用Generator为它们生成对应的ts和py文件
@@ -19,12 +21,16 @@ if __name__ == "__main__":
                 f += glob.glob(f"apis/{i}")
     else:
         f = glob.glob("apis/**/*.xml")
+        shutil.rmtree(Generator.PY_PATH + '/generated')
+        shutil.rmtree(Generator.TS_PATH + '/generated')
+
     if f is not None:
         for file in f:
             if TEST_PATH.match(file) is None:
                 print(file)
                 xml_file = Generator()
                 try:
-                    xml_file.generate(file, file[5:])
+                    output_path = XML2_PATH.sub('', file[5:])
+                    xml_file.generate(file, output_path)
                 except ModelsException as e:
                     print(e)
