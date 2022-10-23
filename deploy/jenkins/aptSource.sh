bc520c7282c8ba0faec5b069443e250bdb12379a
Chlamydomonos <2457242458@qq.com>
2022-10-23 18:58:11 +0800

    换为腾讯云下载源

--- deploy/jenkins/aptSource.sh
+++ deploy/jenkins/aptSource.sh
@@ -4,17 +4,12 @@ sudo apt-get update
 FILE="/etc/apt/sources.list"
 
 cat <<EOM >$FILE
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.tencentyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.tencentyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.tencentyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.tencentyun.com/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.tencentyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.tencentyun.com/ubuntu/ xenial-updates main restricted universe multiverse
 EOM
 
 sudo apt-get update
-sudo apt-get upgrade
