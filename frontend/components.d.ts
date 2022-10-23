a24c4346be35a32eb4337e9335936d3f4d59ab08
Chlamydomonos <2457242458@qq.com>
2022-10-23 17:31:38 +0800

    自动更新的components.d.ts

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -7,11 +7,32 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AreaPreview: typeof import('./src/components/template_editor/AreaPreview.vue')['default']
+    CardEdit: typeof import('./src/components/template_editor/CardEdit.vue')['default']
+    CardPreview: typeof import('./src/components/template_editor/CardPreview.vue')['default']
+    ColumnPreview: typeof import('./src/components/template_editor/ColumnPreview.vue')['default']
+    CreatePropertyDialog: typeof import('./src/components/template_editor/CreatePropertyDialog.vue')['default']
+    EditPropertyDialog: typeof import('./src/components/template_editor/EditPropertyDialog.vue')['default']
+    ElAside: typeof import('element-plus/es')['ElAside']
+    ElAvatar: typeof import('element-plus/es')['ElAvatar']
+    ElButton: typeof import('element-plus/es')['ElButton']
     ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDialog: typeof import('element-plus/es')['ElDialog']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    ElOption: typeof import('element-plus/es')['ElOption']
+    ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElSwitch: typeof import('element-plus/es')['ElSwitch']
+    ElTree: typeof import('element-plus/es')['ElTree']
+    PropertyPreview: typeof import('./src/components/template_editor/PropertyPreview.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    RowPreview: typeof import('./src/components/template_editor/RowPreview.vue')['default']
+    TemplateEditorAside: typeof import('./src/components/template_editor/TemplateEditorAside.vue')['default']
+    TemplateEditorHeader: typeof import('./src/components/template_editor/TemplateEditorHeader.vue')['default']
   }
 }
