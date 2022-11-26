7164a9369bb7a3d661915269ae0af42b5318be59
汪静雅 <wjy_20020731@163.com>
2022-11-27 04:35:37 +0800

    实现人物卡脚本输入的前端

---
+++ frontend/src/components/CardScriptDialog.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <el-form :model="form" :ref="formRef" :rules="rules">
+            <el-form-item label="脚本功能" prop="name">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="脚本描述" prop="description">
+                <el-input
+                    type="textarea"
+                    v-model="form.description"
+                    placeholder="请输入脚本描述"
+                    style="height: 120px"
+                    :rows="5"
+                ></el-input>
+            </el-form-item>
+            <el-form-item>
+                <CardScriptEditor />
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CardScriptEditor from './CardScriptEditor.vue';
+
+export default defineComponent({
+    components: { CardScriptEditor },
+    data() {
+        return {
+            codeContent: '',
+            formRef: 'cardScriptEditor',
+            form: {
+                name: '',
+                description: '',
+            },
+            rules: {
+                name: [
+                    {
+                        required: true,
+                        message: '请输入脚本功能',
+                        trigger: 'blur',
+                    },
+                    {
+                        min: 1,
+                        max: 20,
+                        message: '长度在 1 到 20 个字符',
+                        trigger: 'blur',
+                    },
+                ],
+                description: [
+                    {
+                        required: true,
+                        message: '请输入脚本代码',
+                        trigger: 'blur',
+                    },
+                    {
+                        min: 1,
+                        max: 200,
+                        message: '长度在 1 到 200 个字符',
+                        trigger: 'blur',
+                    },
+                ],
+            },
+        };
+    },
+    methods: {
+        handleChange() {
+            console.log('enter handleChange');
+        },
+    },
+});
+</script>
+
+<style></style>
--- frontend/src/views/CardView.vue
+++ frontend/src/views/CardView.vue
@@ -1,8 +1,57 @@
 <template>
-    <h1>我是Card</h1>
+    <el-button @click="showDialog">show Dialog</el-button>
+    <div id="dialog-cover" style="display: none"></div>
+    <dialog id="dialog-box">
+        <card-script-dialog />
+        <div style="margin-top: 50px; margin-left: 50%">
+            <el-button @click="noDialog">取 消</el-button>
+            <el-button type="primary" @click="noDialog">导 入</el-button>
+        </div>
+    </dialog>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-export default defineComponent({});
+import CardScriptDialog from '../components/CardScriptDialog.vue';
+export default defineComponent({
+    data() {
+        return {};
+    },
+    components: { CardScriptDialog },
+    methods: {
+        showDialog() {
+            //把后面的全都遮盖住
+            document.getElementById('dialog-cover').style.display = 'block';
+            document.getElementById('dialog-box').style.display = 'block';
+            console.log('show');
+        },
+        noDialog() {
+            document.getElementById('dialog-cover').style.display = 'none';
+            document.getElementById('dialog-box').style.display = 'none';
+            console.log('show');
+        },
+    },
+});
 </script>
+
+<style>
+#dialog-cover {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    background-color: #000;
+    opacity: 0.3;
+    filter: alpha(opacity=30);
+}
+#dialog-box {
+    width: 500px;
+    height: 800px;
+    border: none !important;
+    border-radius: calc(5px * var(--ratio));
+    box-shadow: 0 0 #0000, 0 0 #0000, 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+    padding: 1.6rem;
+    max-width: 400px;
+}
+</style>
