c611ecc4f8953f7ac9be31fe74e148d9e071c6be
汪静雅 <wjy_20020731@163.com>
2022-12-20 12:35:37 +0800

    剧本编辑器前端

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -13,6 +13,7 @@ import SocketView from '@/views/SocketView.vue';
 import LogoutView from '@/views/LogoutView.vue';
 import LobbyView from '@/views/LobbyView.vue';
 import RoomView from '@/views/RoomView.vue';
+import NewScriptView from '@/views/NewScript.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +39,7 @@ const router = createRouter({
                 { path: 'card', component: CardView },
                 { path: 'script', component: ScriptView },
                 { path: 'myscript', component: MyScriptView },
+                { path: 'newscript', component: NewScriptView },
                 { path: 'template', component: TemplateCardView },
                 { path: 'mytemplate', component: MyTemplateCardView },
             ],
---
+++ frontend/src/views/NewScript.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>剧本编辑器</h1>
+    <md-editor
+        toolbarsExclude="['mermaid', 'image', 'github']"
+        v-model="text"
+    />
+    <el-button @click="onclick" id="button">提交剧本</el-button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+export default defineComponent({
+    components: { MdEditor },
+    data() {
+        return { text: '' };
+    },
+    methods: {
+        onclick() {
+            // TODO 将剧本中的内容和脚本分别存入后端
+            console.log(this.text);
+            alert('提交成功');
+        },
+    },
+});
+</script>
+
+<style>
+h1 {
+    margin-left: 5px;
+}
+#button {
+    margin-top: 20px;
+    float: right;
+}
+</style>
